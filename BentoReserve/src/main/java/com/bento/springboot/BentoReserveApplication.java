dfb4fab585b8870a84ea2703aa7f601d607811b8
matsuo <1701167@st.asojuku.ac.jp>
2019-10-03 12:04:51 +0900

    弁当予約システム プロジェクト作成

---
+++ BentoReserve/src/main/java/com/bento/springboot/BentoReserveApplication.java
@@ -0,0 +1,13 @@
+package com.bento.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BentoReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BentoReserveApplication.class, args);
+	}
+
+}
